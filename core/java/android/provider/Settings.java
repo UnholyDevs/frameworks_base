3dd2553a0ad370f6e1e09e373357f7d82e06888a
Danesh M <daneshm90@gmail.com>
2015-01-18 00:33:00 -0800

    SystemUI: Lockscreen shortcut customization Squashed with: Fix leftAffordanceView visibility bug 121c7a982cd88bef53b79baf3665db9c415281f8 LockscreenShortcuts: Since Voice assist is default use its icon 7e6c2ebf86fe95300705ce5da847ac0ee855e1b7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7501,6 +7501,12 @@ public final class Settings {
         public static final String QQS_COUNT = "sysui_qqs_count";
 
         /**
+         * Launch actions for left/right lockscreen targets
+         * @hide
+         */
+        public static final String LOCKSCREEN_TARGETS = "lockscreen_target_actions";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
