6005c1a8aa6e2354dc5fbb9fc125e2376b746992
beanstown106 <nbenis106@gmail.com>
2015-06-28 16:39:26 -0400

    StatusbarHeader: make user avatar long clickable
    everything else is long clickable might as well make long click on avatar bring us to user settings

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -178,6 +178,7 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
         mAlarmStatus.setOnClickListener(this);
 
         mMultiUserSwitch = (MultiUserSwitch) findViewById(R.id.multi_user_switch);
+        mMultiUserSwitch.setOnLongClickListener(this);
         mMultiUserAvatar = (ImageView) mMultiUserSwitch.findViewById(R.id.multi_user_avatar);
 
         // RenderThread is doing more harm than good when touching the header (to expand quick
@@ -437,6 +438,8 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
             startClockLongClickActivity();
         } else if (v == mDate) {
             startDateLongClickActivity();
+        } else if (v == mMultiUserSwitch) {
+            startUserLongClickActivity();
         }
         return false;
     }
@@ -467,6 +470,13 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
     private void startDateLongClickActivity() {
         Intent intent = new Intent(Intent.ACTION_INSERT);
             intent.setData(Events.CONTENT_URI);
+        mActivityStarter.startActivity(intent, true /* dismissShade */);
+    }
+
+    private void startUserLongClickActivity() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setClassName("com.android.settings",
+            "com.android.settings.Settings$UserSettingsActivity");
         mActivityStarter.startActivity(intent, true /* dismissShade */);
     }
 
