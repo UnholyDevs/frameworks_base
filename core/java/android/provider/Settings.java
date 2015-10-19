5917fb87ae85d196aa4a5e95186087e4db51903e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2015-10-19 03:02:28 -0400

    Configurable 0, 90, 180 and 270 degree rotation
    - Add system setting
    - Add longpress link to advanced rotation menu

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4303,6 +4303,19 @@ public final class Settings {
         public static final String AMBIENT_DOZE_AUTO_BRIGHTNESS = "ambient_doze_auto_brightness";
 
         /**
+         * Control the type of rotation which can be performed using the accelerometer
+         * if ACCELEROMETER_ROTATION is enabled.
+         * Value is a bitwise combination of
+         * 1 = 0 degrees (portrait)
+         * 2 = 90 degrees (left)
+         * 4 = 180 degrees (inverted portrait)
+         * 8 = 270 degrees (right)
+         * Setting to 0 is effectively orientation lock
+		 * @hide
+		 */
+        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
