83ded8b2e970600bd33795c593322f9ac6ac924b
Peter Schiller <peterschiller@google.com>
2015-11-18 14:22:40 -0800

    better notification speed change
    Change-Id: Ib2530037fd697537216dbc4cc73f4212b307363a
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -108,7 +108,6 @@ import com.android.systemui.statusbar.NotificationGuts.OnGutsClosedListener;
 import com.android.systemui.statusbar.notification.VisualStabilityManager;
 import com.android.systemui.statusbar.phone.NavigationBarView;
 import com.android.systemui.statusbar.phone.NotificationGroupManager;
-import com.android.systemui.statusbar.phone.NotificationPanelView;
 import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
 import com.android.systemui.statusbar.policy.PreviewInflater;
@@ -2029,8 +2028,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
                     // close the shade if it was open
                     animateCollapsePanels(CommandQueue.FLAG_EXCLUDE_RECENTS_PANEL,
-                            true /* force */, true /* delayed */,
-                            NotificationPanelView.SPEED_UP_FACTOR_CLICKED);
+                            true /* force */, true /* delayed */);
                     visibilityChanged(false);
 
                     return true;
@@ -2063,10 +2061,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     public void animateCollapsePanels(int flags, boolean force, boolean delayed) {
-    }
-
-    public void animateCollapsePanels(int flags, boolean force, boolean delayed,
-            float speedUpFactor) {
     }
 
     public void overrideActivityPendingAppTransition(boolean keyguardShowing) {
