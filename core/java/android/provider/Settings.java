2fcbd7dd6b9e34321eefec0eb223aecf00dd8203
Alex <mazdarider23@gmail.com>
2016-09-12 17:55:33 -0400

    Disable dashboard suggestions [2/2]
    Change-Id: I33879e9b8e1ef15898e781aad4766b755df36287
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4095,6 +4095,12 @@ public final class Settings {
         public static final String DISABLE_DASHBOARD_CONDITIONS = "disable_dashboard_conditions";
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
