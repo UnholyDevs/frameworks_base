225eb830af29bc090a37ee4e958e8c1f52bf60ad
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-09-26 00:14:26 +0200

    Screenshot QS tile
    [QUICK SETTINGS] Add Screenshot QS Tile
    
    Change-Id: I3392504d18d4c5f191f9f29c81dbe4d9db228890
    
    [QUICK SETTINGS] Update screenshot QS icon
    
    Change-Id: I4cebf97638edf13c71e48dcefbd58924be49d36b
    
    Add screenshot tile to config

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -60,6 +60,7 @@ import com.android.systemui.qs.tiles.LockscreenToggleTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -448,6 +449,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("sound")) return new SoundTile(this);
         else if (tileSpec.equals("lockscreen")) return  new LockscreenToggleTile(this);
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
+        else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
