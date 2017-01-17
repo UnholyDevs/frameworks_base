c0634bb2fd169005e1774514895804311c12b7b8
beanstown106 <nbenis106@gmail.com>
2017-01-16 22:27:17 -0500

    One handed mode toggle [1/3]
    Change-Id: I8e916c60477de87766c5946043e3db46f523aa49

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7151,6 +7151,12 @@ public final class Settings {
         public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
         /**
+         * One Handed mode toggle
+         * @hide
+         */
+        public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
