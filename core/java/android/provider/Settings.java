fcf1b9002c89318e414851e9b0b47af8eb97fc8d
maxwen <max.weninger@gmail.com>
2017-01-21 10:11:52 +0100

    Dashboard tile layout config [2/2]
    Change-Id: I387b0ecfec5bd71e8c1176b06a83796d09aba3e4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4101,6 +4101,12 @@ public final class Settings {
         public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
 
         /**
+         * Dashboard columns in settings
+         * @hide
+         */
+        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
