891ce5b245ac63acca8dd956412249f1fd38278e
Alex Cruz <mazdarider23@gmail.com>
2017-01-22 09:42:55 +0100

    Rework dashboard settings [2/2]
    Change-Id: I91221badb75ebd755479ee0de8f0e88026109e6d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4101,16 +4101,22 @@ public final class Settings {
         public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
 
         /**
-         * Dashboard columns in settings
+         * Dashboard portrait columns in settings
          * @hide
          */
-        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+        public static final String DASHBOARD_PORTRAIT_COLUMNS = "dashboard_portrait_columns";
 
         /**
-         * Whether to show dashboard summaries in a single line or not
+         * Remove tile summaries in settings
          * @hide
          */
-        public static final String DASHBOARD_SUMMARY_DOUBLE_LINES = "dashboard_summary_double_lines";
+        public static final String REMOVE_TILE_SUMMARY = "remove_tile_summary";
+
+        /**
+         * Dashboard landscape columns in settings
+         * @hide
+         */
+        public static final String DASHBOARD_LANDSCAPE_COLUMNS = "dashboard_landscape_columns";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
