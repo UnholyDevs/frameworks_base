ae7ea5dba8501c02e335ee0657f157271d0f98e4
George G <kreach3r@users.noreply.github.com>
2017-02-25 17:32:18 +0200

    base: Force authorize all calling packages by Masquerade (2/3)
    Change-Id: Iaf354c4d891acf9f5e19767223e4f9860a07338f
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3957,6 +3957,12 @@ public final class Settings {
         public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by Masquerade
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
