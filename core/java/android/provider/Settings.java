03a68c2526929d20ffa0f5349af6f5e7371b6707
jl90 <leonardjason90.jl@gmail.com>
2017-02-27 23:31:58 +0000

    Revert "Lockscreen Clock Widget Fonts [1/2]"
    This reverts commit d79048e4c891bf2e4f92126aeaa13e342b8276a5.
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3678,12 +3678,6 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
-         * Change fonts for the system lockscreen clock widget
-         * @hide
-         */
-        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
-
-        /**
          * whether to enable torch on lockscreen
          * @hide
          */
