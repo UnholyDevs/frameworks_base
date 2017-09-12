72a31289122c9e1f11f8d0299b010dad298ea33a
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to UnholyUtils

--- core/java/com/android/internal/util/unholy/UnholyUtils.java
+++ core/java/com/android/internal/util/unholy/UnholyUtils.java
@@ -19,13 +19,22 @@ package com.android.internal.util.unholy;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 import java.util.Locale;
 
-public class unholyUtils {
+public class UnholyUtils {
 
-    public static boolean isChineseLanguage() {
-       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
-               Locale.CHINESE.getLanguage());
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
